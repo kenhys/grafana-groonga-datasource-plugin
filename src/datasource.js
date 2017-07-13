55b6c22417cac4cbe6364ab6ae691f398ce94d02
Kentaro Hayashi <hayashi@clear-code.com>
2017-07-13 16:45:50 +0900

    Import query implementation from g-d-p-g

--- src/datasource.js
+++ src/datasource.js
@@ -16,6 +16,49 @@ export class GroongaDatasource {
   }
 
   query(options) {
+    var target = options.targets[0];
+    var column = target.column;
+    var timeColumn;
+    if (target.timeColumns.indexOf('timestamp') >= 0) {
+      timeColumn = 'timestamp';
+    } else {
+      timeColumn = target.timeColumns[0];
+    }
+    var selectOptions = {
+      table: target.table,
+      output_columns: timeColumn + ', ' + target.column,
+      filter: 'between(' + timeColumn + ', ' +
+                       options.range.from.unix() + ', "include", ' +
+                       options.range.to.unix() + ', "include")',
+      limit: -1
+    };
+    var requestOptions = {
+      url: this.datasource.url + '/d/select?' + params(selectOptions)
+    };
+    return backendSrv.datasourceRequest(requestOptions).then(function(result) {
+      var data = [];
+      var seriesSet = {};
+      var select = result.data[1][0];
+      var i;
+      for (i = 2; i < select.length; i++) {
+        var record = select[i];
+        var timestamp = record[0];
+        var value = record[1];
+        var series = seriesSet[column];
+        var datapoints;
+        if (!series) {
+          series = seriesSet[column] = {
+            target: column,
+            datapoints: []
+          };
+          data.push(series);
+        }
+        datapoints = series.datapoints;
+        datapoints.push([value, timestamp * 1000]);
+      }
+      return {data: data};
+    });
+/*
     var query = this.buildQueryParameters(options);
     query.targets = query.targets.filter(t => !t.hide);
 
@@ -29,6 +72,7 @@ export class GroongaDatasource {
       method: 'POST',
       headers: this.headers
     });
+*/
   }
 
   testDatasource() {
